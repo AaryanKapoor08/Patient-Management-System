8e4e9708b152a3d85600a30ae59a2adbaf8c9fa2
Priyan Arora <Priyanarora22@gmail.com>
2026-01-16 06:13:24 -0400

    Commiting 1st microservice

---
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -0,0 +1,62 @@
+package com.pm.patientservice.controller;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroups;
+import com.pm.patientservice.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/patients")
+@Tag(name = "Patient", description = "API for managing patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    @Operation(summary = "Get patients")
+    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+        List<PatientResponseDTO> patients =  patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+    //i still aint getting a co-op
+    @PostMapping
+    @Operation(summary = "Create a new patient")
+    public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class, CreatePatientValidationGroups.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+
+    //everytime we put a putmapping, we put in association with some id i.e what we wanna change
+    //i.e localhost:4000/patients/1231232-1212132-1133121
+    @PutMapping("/{id}")
+    @Operation(summary = "Update a patient")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
+        //default class to validate if requestbody is valid or not
+        PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
+
+        return  ResponseEntity.ok().body(patientResponseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a patient")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+        //will return response entity but nothing to return
+        patientService.deletePatient(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,52 @@
+package com.pm.patientservice.dto;
+
+public class PatientResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String dateOfBirth;
+    //no need to send registred date to frontend in form of Dto
+    //primitive types compared to patient class for easier handling
+}
